ea4bb567b79bba13677b3b50a6ecbec0a99499b1
myjdesert <50020138+myjdesert@users.noreply.github.com>
2026-05-17 00:14:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,18 +2,11 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 终极防御：在虚拟机根目录下提前创建一个假的 swapfile 并剥夺其所有权限，彻底废掉源码脚本对它的作死操作
+sudo touch /swapfile
+sudo chmod 000 /swapfile
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 引入你需要的最新 Lucky、PassWall 插件源
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
