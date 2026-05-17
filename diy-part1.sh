ccfa8ed35edd79b43868c2aadff1713d062881d2
myjdesert <50020138+myjdesert@users.noreply.github.com>
2026-05-17 11:53:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 #
 
-# 引入你需要的最新 Lucky、PassWall 插件源
+# 1. 引入常规的额外插件源（Lucky, PassWall等）
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# 2. 💥 绝杀：强制把专用于高通 qualcommax/ipq60xx 的核心机型支持库克隆并合并到源码中
+# 确保 make defconfig 运行时能够认识并完美匹配 ZN-M2 兆能核心
+git clone https://github.com/VikingYFY/immortalwrt --depth=1 -b openwrt-23.05/target/linux/qualcommax /tmp/qualcommax
+rm -rf target/linux/qualcommax
+mv /tmp/qualcommax/target/linux/qualcommax target/linux/
